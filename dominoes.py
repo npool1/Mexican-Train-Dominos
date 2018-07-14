7b47b4a4c05b052d75867bac727f52ea2e0ae3ec
npool1 <nicholaspool@gmail.com>
2018-07-13 17:01:43 -0700

    deckbuild functionality added

--- dominoes.py
+++ dominoes.py
@@ -3,17 +3,29 @@ class Tile(object):
         self.suit1 = suit1
         self.suit2 = suit2
 
-    def getSuits():
-        return tuple(suit1, suit2)
+    def getSuits(self):
+        print("{} : {}".format(self.suit1, self.suit2))
+        # return tuple(suit1, suit2)
 
     def getPipCount():
         return suit1 + suit2
     
-class Dominoes(object):
+class Deck(object):
     def __init__(self):
-        self.deck = shuffle()
+        self.tiles = []
+        self.build()
+        # self.shuffle()
 
     # Creates a new deck of 12:12 dominoes and shuffles the deck
-    def shuffle(self):
-        deck = []
-        for i in range(0, 12):
+    def build(self):
+        for i in range(0, 13):
+            for j in range(i, 13):
+                self.tiles.append(Tile(i,j))
+
+    def show(self):
+        for t in self.tiles:
+            t.getSuits()
+
+deck = Deck()
+deck.show()
+
