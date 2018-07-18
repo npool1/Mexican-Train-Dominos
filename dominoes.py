e79bfc8f265638bc6900294839bc44066e999a8f
npool1 <nicholaspool@gmail.com>
2018-07-18 14:45:16 -0700

    added shuffle functionality

--- dominoes.py
+++ dominoes.py
@@ -1,3 +1,4 @@
+import random
 class Tile(object):
     def __init__(self, suit1, suit2):
         self.suit1 = suit1
@@ -5,16 +6,16 @@ class Tile(object):
 
     def getSuits(self):
         print("{} : {}".format(self.suit1, self.suit2))
-        # return tuple(suit1, suit2)
+        # return tuple(self.suit1, self.suit2)
 
     def getPipCount():
         return suit1 + suit2
-    
+
 class Deck(object):
     def __init__(self):
         self.tiles = []
         self.build()
-        # self.shuffle()
+        self.shuffle()
 
     # Creates a new deck of 12:12 dominoes and shuffles the deck
     def build(self):
@@ -25,7 +26,20 @@ class Deck(object):
     def show(self):
         for t in self.tiles:
             t.getSuits()
+    
+    def shuffle(self):
+        for i in range(len(self.tiles)-1,0,-1):
+            r = random.randint(0, i)
+            self.tiles[i], self.tiles[r] = self.tiles[r], self.tiles[i]
+    
+    def draw(self):
+        return self.tiles.pop()
 
 deck = Deck()
-deck.show()
+# deck.show()
+# deck.shuffle()
+tile = deck.draw()
+tile.getSuits()
+# deck.show()
+
 
